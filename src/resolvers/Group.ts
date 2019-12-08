d7ceaf6e33de9ed584c812747d9877c7c5e96c42
svirins <svirins@gmail.com>
2019-12-08 10:25:23 +0300

    ready - but got typescript error on geometry

---
+++ src/resolvers/Group.ts
@@ -0,0 +1,18 @@
+// This resolver file was scaffolded by github.com/prisma/graphqlgen, DO NOT EDIT.
+// Please do not import this file directly but copy & paste to your application code.
+
+import { GroupResolvers } from "../generated/graphqlgen";
+
+export const Group: GroupResolvers.Type = {
+  ...GroupResolvers.defaultResolvers,
+
+  author: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  },
+  geodata: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  },
+  meetings: (parent, args, ctx) => {
+    throw new Error("Resolver not implemented");
+  }
+};
